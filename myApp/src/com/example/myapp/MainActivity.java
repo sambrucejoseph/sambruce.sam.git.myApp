035354d2c594dbf4ee35b2e72cbafac091445fc4
Sambruce Sam <sambruce23@gmail.com>
2013-01-21 13:32:06 +0800

    First commit! Yay!

---
+++ myApp/src/com/example/myapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.setIntegerProperty("splashscreen", R.drawable.splash);
+		super.loadUrl("file:///android_asset/www/index.html", 10000);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
