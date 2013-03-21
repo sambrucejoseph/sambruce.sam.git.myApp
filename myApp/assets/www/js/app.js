eb6932cadf0f37b658d6d51a4555fba089477c89
Sambruce Sam <sambruce23@gmail.com>
2013-03-21 13:40:58 +0800

    new css

---
+++ myApp/assets/www/js/app.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+//Init IScroll
+var theScroll;
+function scroll(){
+    theScroll = new iScroll('tab-content');
+}
+document.addEventListener('DOMContentLoaded', scroll, false);
